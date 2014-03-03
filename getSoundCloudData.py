af5b01908a0ca917fcf1727487bb86f02289f3f7
annajordanous <annajordanous@hotmail.com>
2014-03-03 10:39:26 +0000

    Matching export db table structure against SoundCloud data structures

--- getSoundCloudData.py
+++ getSoundCloudData.py
@@ -176,173 +176,153 @@ def exportDataToSQLite():
         cursor = db.cursor()
         # Check if table users does not exist and create it
         cursor.execute('''CREATE TABLE IF NOT EXISTS
-                      agents(id INTEGER PRIMARY KEY, name TEXT, phone TEXT, email TEXT unique, password TEXT)''')
+                      agents(id INTEGER PRIMARY KEY, permalink TEXT, username TEXT, uri TEXT,
+                             permalink_url TEXT, avatar_url TEXT, country TEXT, full_name TEXT, 
+                             city TEXT, description TEXT, discogs_name TEXT, myspace_name TEXT,
+                             website TEXT, website_title TEXT, online TEXT, track_count INTEGER,
+                             playlist_count INTEGER, followers_count INTEGER, 
+                             followings_count INTEGER, public_favorites_count INTEGER)''')
         for agent in agents:
-            cursor.execute('''INSERT INTO agents(id, permalink, username, uri, permalink_url, avatar_url, country, full_name, city, description, discogs_name) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', (x.id, x.permalink, x.username, x.uri, x.permalink_url, x.avatar_url, x.country, x.full_name, x.city, x.description, x.discogs_name))
-    #  Users  {
-#  "id": 3207,
-#  "permalink": "jwagener",
-#  "username": "Johannes Wagener",
-#  "uri": "http://api.soundcloud.com/users/3207",
-#  "permalink_url": "http://soundcloud.com/jwagener",
-#  "avatar_url": "http://i1.sndcdn.com/avatars-000001552142-pbw8yd-large.jpg?142a848",
-#  "country": "Germany",
-#  "full_name": "Johannes Wagener",
-#  "city": "Berlin",
-#  "description": "<b>Hacker at SoundCloud</b>\r\n\r\nSome of my recent Hacks:\r\n\r\nsoundiverse.com \r\nbrowse recordings with the FiRe app by artwork\r\n\r\ntopbillin.com \r\nfind people to follow on SoundCloud\r\n\r\nchatter.fm \r\nget your account hooked up with a voicebox\r\n\r\nrecbutton.com \r\nrecord straight to your soundcloud account",
-#  "discogs_name": null,
-#  "myspace_name": null,
-#  "website": "http://johannes.wagener.cc",
-#  "website_title": "johannes.wagener.cc",
-#  "online": true,
-#  "track_count": 12,
-#  "playlist_count": 1,
-#  "followers_count": 417,
-#  "followings_count": 174,
-#  "public_favorites_count": 26
-#}
-#id     integer ID     123
-#permalink     permalink of the resource     "sbahn-sounds"
-#username     username     "Doctor Wilson"
-#uri     API resource URL     http://api.soundcloud.com/comments/32562
-#permalink_url     URL to the SoundCloud.com page     "http://soundcloud.com/bryan/sbahn-sounds"
-#avatar_url     URL to a JPEG image     "http://i1.sndcdn.com/avatars-000011353294-n0axp1-large.jpg"
-#country     country     "Germany"
-#full_name     first and last name     "Tom Wilson"
-#city     city     "Berlin"
-#description     description     "Buskers playing in the S-Bahn station in Berlin"
-#discogs-name     Discogs name     "myrandomband"
-#myspace-name     MySpace name     "myrandomband"
-#website     a URL to the website     "http://facebook.com/myrandomband"
-#website-title     a custom title for the website     "myrandomband on Facebook"
-#online     online status (boolean)     true
-#track_count     number of public tracks     4
-#playlist_count     number of public playlists     5
-#followers_count     number of followers     54
-#followings_count     number of followed users     75
-#public_favorites_count     number of favorited public tracks     7
-#avatar_data     binary data of user avatar     (only for uploading)
+            # USERS table: fields, description and example value:
+            #id     integer ID     123
+            #permalink     permalink of the resource     "sbahn-sounds"
+            #username     username     "Doctor Wilson"
+            #uri     API resource URL     http://api.soundcloud.com/comments/32562
+            #permalink_url     URL to the SoundCloud.com page     "http://soundcloud.com/bryan/sbahn-sounds"
+            #avatar_url     URL to a JPEG image     "http://i1.sndcdn.com/avatars-000011353294-n0axp1-large.jpg"
+            #country     country     "Germany"
+            #full_name     first and last name     "Tom Wilson"
+            #city     city     "Berlin"
+            #description     description     "Buskers playing in the S-Bahn station in Berlin"
+            #discogs-name     Discogs name     "myrandomband"
+            #myspace-name     MySpace name     "myrandomband"
+            #website     a URL to the website     "http://facebook.com/myrandomband"
+            #website-title     a custom title for the website     "myrandomband on Facebook"
+            #online     online status (boolean)     true
+            #track_count     number of public tracks     4
+            #playlist_count     number of public playlists     5
+            #followers_count     number of followers     54
+            #followings_count     number of followed users     75
+            #public_favorites_count     number of favorited public tracks     7
+            #avatar_data     binary data of user avatar     (only for uploading)
+            cursor.execute('''INSERT INTO agents(id, permalink, username, uri,
+                             permalink_url, avatar_url, country, full_name, 
+                             city, description, discogs_name, myspace_name,
+                             website, website_title, online, track_count,
+                             playlist_count, followers_count, 
+                             followings_count, public_favorites_count) 
+                             VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', 
+                             (agent.id, agent.permalink, agent.username, agent.uri, agent.permalink_url, 
+                              agent.avatar_url, agent.country, agent.full_name, agent.city, 
+                              agent.description, agent.discogs_name, agent.myspace_name, agent.website, 
+                              agent.website_title, agent.online, agent.track_count, agent.playlist_count, 
+                              agent.followers_count, agent.followings_count, agent.public_favorites_count))
 
-# x_follows_y set of tuples (follower.id, followed.id) 
-        cursor.execute('''CREATE TABLE IF NOT EXISTS x_follows_y(id INTEGER PRIMARY KEY, name TEXT,
-                                                                 phone TEXT, email TEXT unique, password TEXT)''')
+        # X FOLLOWS Y set of tuples (follower.id, followed.id) 
+        cursor.execute('''CREATE TABLE IF NOT EXISTS x_follows_y(follower INTEGER PRIMARY KEY, followed INTEGER PRIMARY KEY)''')
         for follow in x_follows_y:
-            cursor.execute('''INSERT INTO x_follows_y(id, permalink, username, uri, permalink_url, 
-                              avatar_url, country, full_name, city, description, discogs_name) 
-                              VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', 
-                              (x.id, x.permalink, x.username, x.uri, x.permalink_url, x.avatar_url, 
-                               x.country, x.full_name, x.city, x.description, x.discogs_name))
-# TRACKS
-        cursor.execute('''CREATE TABLE IF NOT EXISTS tracks(id INTEGER PRIMARY KEY, name TEXT, 
-        phone TEXT, email TEXT unique, password TEXT)''')
+            cursor.execute('''INSERT INTO x_follows_y(follower, followed) 
+                              VALUES(?, ?)''', 
+                              (follow[0], follow[1]))
+        # TRACKS
+        #id     integer ID     123
+        #created_at     timestamp of creation     "2009/08/13 18:30:10 +0000"
+        #user_id     user-id of the owner     343
+        #user     mini user representation of the owner     {id: 343, username: "Doctor Wilson"...}
+        #title     track title     "S-Bahn Sounds"
+        #permalink     permalink of the resource     "sbahn-sounds"
+        #permalink_url     URL to the SoundCloud.com page     "http://soundcloud.com/bryan/sbahn-sounds"
+        #uri     API resource URL     "http://api.soundcloud.com/tracks/123"
+        #sharing     public/private sharing     "public"
+        #embeddable_by     who can embed this track or playlist     "all", "me", or "none"
+        #purchase_url     external purchase link     "http://amazon.com/buy/a43aj0b03"
+        #artwork_url     URL to a JPEG image     "http://i1.sndcdn.com/a....-large.jpg?142a848"
+        #description     HTML description     "my first track"
+        #label     label mini user object     {id:123, username: "BeatLabel"...}
+        #duration     duration in milliseconds     1203400
+        #genre     genre     "HipHop"
+        #shared_to_count     number of sharings (if private)     45
+        #tag_list     list of tags     "tag1 \"hip hop\" geo:lat=32.444 geo:lon=55.33"
+        #label_id     id of the label user     54677
+        #label_name     label name     "BeatLabel"
+        #release     release number     3234
+        #release_day     day of the release     21
+        #release_month     month of the release     5
+        #release_year     year of the release     2001
+        #streamable     streamable via API (boolean)     true
+        #downloadable     downloadable (boolean)     true
+        #state     encoding state     "finished"
+        #license     creative common license     "no-rights-reserved"
+        #track_type     track type     "recording"
+        #waveform_url     URL to PNG waveform image     "http://w1.sndcdn.com/fxguEjG4ax6B_m.png"
+        #download_url     URL to original file     "http://api.soundcloud.com/tracks/3/download"
+        #stream_url     link to 128kbs mp3 stream     "http://api.soundcloud.com/tracks/3/stream"
+        #video_url     a link to a video page     "http://vimeo.com/3302330"
+        #bpm     beats per minute     120
+        #commentable     track commentable (boolean)     true
+        #isrc     track ISRC     "I123-545454"
+        #key_signature     track key     "Cmaj"
+        #comment_count     track comment count     12
+        #download_count     track download count     45
+        #playback_count     track play count     435
+        #favoritings_count     track favoriting count     6
+        #original_format     file format of the original file     "aiff"
+        #original_content_size     size in bytes of the original file     10211857
+        #created_with     the app that the track created     {"id"=>3434, "..."=>nil}
+        ####asset_data     binary data of the audio file     (only for uploading)
+        ####artwork_data     binary data of the artwork image     (only for uploading)
+        #user_favorite     track favorite of current user (boolean, authenticated requests only)
+        cursor.execute('''CREATE TABLE IF NOT EXISTS tracks(id INTEGER PRIMARY KEY, 
+        created_at TEXT, 
+        user_id INTEGER, user_permalink TEXT, user_username TEXT, user_uri TEXT, 
+        user_permalink_url TEXT, user_avatar_url TEXT, 
+        title TEXT, permalink TEXT, permalink_url TEXT, uri TEXT, sharing TEXT, 
+        embeddable_by TEXT, purchase_url TEXT, artwork_url TEXT,  description TEXT, 
+        label TEXT, duration INTEGER, genre TEXT, shared_to_count INTEGER, 
+        tag_list TEXT, label_id INTEGER, label_name TEXT, release INTEGER, 
+        release_day INTEGER, release_month INTEGER, release_year INTEGER, 
+        streamable TEXT, downloadable TEXT, state TEXT, license TEXT, 
+        track_type TEXT, waveform_url TEXT, download_url TEXT, stream_url TEXT, 
+        video_url TEXT, bpm INTEGER, commentable TEXT, isrc TEXT, 
+        key_signature TEXT, comment_count INTEGER, download_count INTEGER, 
+        playback_count INTEGER, favoritings_count INTEGER, original_format TEXT, 
+        original_content_size INTEGER, 
+        created_with_id INTEGER, created_with_name TEXT, created_with_uri TEXT, 
+        created_with_permalink_url TEXT, user_favorite TEXT)''')   
         for track in tracks:
-            cursor.execute('''INSERT INTO agents(id, permalink, username, uri, permalink_url, avatar_url, 
-            country, full_name, city, description, discogs_name) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', 
-            (x.id, x.permalink, x.username, x.uri, x.permalink_url, x.avatar_url, x.country, 
-             x.full_name, x.city, x.description, x.discogs_name))
-#{
-#  "id": 13158665,
-#  "created_at": "2011/04/06 15:37:43 +0000",
-#  "user_id": 3699101,
-#  "duration": 18109,
-#  "commentable": true,
-#  "state": "finished",
-#  "sharing": "public",
-#  "tag_list": "soundcloud:source=iphone-record",
-#  "permalink": "munching-at-tiannas-house",
-#  "description": null,
-#  "streamable": true,
-#  "downloadable": true,
-#  "genre": null,
-#  "release": null,
-#  "purchase_url": null,
-#  "label_id": null,
-#  "label_name": null,
-#  "isrc": null,
-#  "video_url": null,
-#  "track_type": "recording",
-#  "key_signature": null,
-#  "bpm": null,
-#  "title": "Munching at Tiannas house",
-#  "release_year": null,
-#  "release_month": null,
-#  "release_day": null,
-#  "original_format": "m4a",
-#  "original_content_size": 10211857,
-#  "license": "all-rights-reserved",
-#  "uri": "http://api.soundcloud.com/tracks/13158665",
-#  "permalink_url": "http://soundcloud.com/user2835985/munching-at-tiannas-house",
-#  "artwork_url": null,
-#  "waveform_url": "http://w1.sndcdn.com/fxguEjG4ax6B_m.png",
-#  "user": {
-#    "id": 3699101,
-#    "permalink": "user2835985",
-#    "username": "user2835985",
-#    "uri": "http://api.soundcloud.com/users/3699101",
-#    "permalink_url": "http://soundcloud.com/user2835985",
-#    "avatar_url": "http://a1.sndcdn.com/images/default_avatar_large.png?142a848"
-#  },
-#  "stream_url": "http://api.soundcloud.com/tracks/13158665/stream",
-#  "download_url": "http://api.soundcloud.com/tracks/13158665/download",
-#  "playback_count": 0,
-#  "download_count": 0,
-#  "favoritings_count": 0,
-#  "comment_count": 0,
-#  "created_with": {
-#    "id": 124,
-#    "name": "SoundCloud iPhone",
-#    "uri": "http://api.soundcloud.com/apps/124",
-#    "permalink_url": "http://soundcloud.com/apps/iphone"
-#  },
-#  "attachments_uri": "http://api.soundcloud.com/tracks/13158665/attachments"
-#}
-#id     integer ID     123
-#created_at     timestamp of creation     "2009/08/13 18:30:10 +0000"
-#user_id     user-id of the owner     343
-#user     mini user representation of the owner     {id: 343, username: "Doctor Wilson"...}
-#title     track title     "S-Bahn Sounds"
-#permalink     permalink of the resource     "sbahn-sounds"
-#permalink_url     URL to the SoundCloud.com page     "http://soundcloud.com/bryan/sbahn-sounds"
-#uri     API resource URL     "http://api.soundcloud.com/tracks/123"
-#sharing     public/private sharing     "public"
-#embeddable_by     who can embed this track or playlist     "all", "me", or "none"
-#purchase_url     external purchase link     "http://amazon.com/buy/a43aj0b03"
-#artwork_url     URL to a JPEG image     "http://i1.sndcdn.com/a....-large.jpg?142a848"
-#description     HTML description     "my first track"
-#label     label mini user object     {id:123, username: "BeatLabel"...}
-#duration     duration in milliseconds     1203400
-#genre     genre     "HipHop"
-#shared_to_count     number of sharings (if private)     45
-#tag_list     list of tags     "tag1 \"hip hop\" geo:lat=32.444 geo:lon=55.33"
-#label_id     id of the label user     54677
-#label_name     label name     "BeatLabel"
-#release     release number     3234
-#release_day     day of the release     21
-#release_month     month of the release     5
-#release_year     year of the release     2001
-#streamable     streamable via API (boolean)     true
-#downloadable     downloadable (boolean)     true
-#state     encoding state     "finished"
-#license     creative common license     "no-rights-reserved"
-#track_type     track type     "recording"
-#waveform_url     URL to PNG waveform image     "http://w1.sndcdn.com/fxguEjG4ax6B_m.png"
-#download_url     URL to original file     "http://api.soundcloud.com/tracks/3/download"
-#stream_url     link to 128kbs mp3 stream     "http://api.soundcloud.com/tracks/3/stream"
-#video_url     a link to a video page     "http://vimeo.com/3302330"
-#bpm     beats per minute     120
-#commentable     track commentable (boolean)     true
-#isrc     track ISRC     "I123-545454"
-#key_signature     track key     "Cmaj"
-#comment_count     track comment count     12
-#download_count     track download count     45
-#playback_count     track play count     435
-#favoritings_count     track favoriting count     6
-#original_format     file format of the original file     "aiff"
-#original_content_size     size in bytes of the original file     10211857
-#created_with     the app that the track created     {"id"=>3434, "..."=>nil}
-#asset_data     binary data of the audio file     (only for uploading)
-#artwork_data     binary data of the artwork image     (only for uploading)
-#user_favorite     track favorite of current user (boolean, authenticated requests only)     1
+            cursor.execute('''INSERT INTO tracks(id, created_at, 
+        user_id, user_permalink, user_username, user_uri, 
+        user_permalink_url, user_avatar_url, 
+        title, permalink, permalink_url, uri, sharing, 
+        embeddable_by, purchase_url, artwork_url,  description, 
+        label, duration, genre, shared_to_count, 
+        tag_list, label_id, label_name, release, 
+        release_day, release_month, release_year, 
+        streamable, downloadable, state, license, 
+        track_type, waveform_url, download_url, stream_url, 
+        video_url, bpm, commentable, isrc, 
+        key_signature, comment_count, download_count, 
+        playback_count, favoritings_count, original_format, 
+        original_content_size, 
+        created_with_id, created_with_name, created_with_uri, 
+        created_with_permalink_url, user_favorite) 
+        VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 
+        ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', 
+            (track.id, track.created_at, track.user_id, track.user_permalink, 
+             track.user_username, track.user_uri, track.user_permalink_url, 
+             track.user_avatar_url, track.title, track.permalink, 
+             track.permalink_url, track.uri, track.sharing, track.embeddable_by, 
+             track.purchase_url, track.artwork_url, track. description, track.label, 
+             track.duration, track.genre, track.shared_to_count, track.tag_list, 
+             track.label_id, track.label_name, track.release, track.release_day, 
+             track.release_month, track.release_year, track.streamable, 
+             track.downloadable, track.state, track.license, track.track_type, 
+             track.waveform_url, track.download_url, track.stream_url, track.video_url, 
+             track.bpm, track.commentable, track.isrc, track.key_signature, 
+             track.comment_count, track.download_count, track.playback_count, 
+             track.favoritings_count, track.original_format, track.original_content_size, 
+             track.created_with_id, track.created_with_name, track.created_with_uri, 
+             track.created_with_permalink_url, track.user_favorite))
         db.commit()
     # Catch the exception
     except Exception as e:
