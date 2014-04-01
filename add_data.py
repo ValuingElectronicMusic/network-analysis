783050f9c380224364f2ad75b2abdb8a1b1f6ff0
daniel-allington <daniel.allington@open.ac.uk>
2014-04-01 22:21:33 +0100

    Code for adding data to sqlite database
    This is supposed to avoid the bugs that were coming up with SoundCloud
    objects missing particular attributes: it adds 'NULL' if an attribute
    is missing. But it's also supposed to reduce headaches in another way,
    because it generalises the code for adding rows to tables in the
    database. You need to create two strings per table, one used for the
    SQL statement that creates the table, and one which contains all the
    attributes that will be put into columns in the table (this will be the
    same as the previous string, minus all the capital letters! I guess I
    could have written a regex to convert one to the other, but… it's late).

---
+++ add_data.py
@@ -0,0 +1,94 @@
+import sqlite3
+
+dummy_att_str='id, user_id, title'
+dummy_table_creator='id INTEGER, user_id INTEGER, title TEXT'
+
+
+track_att_str='''id, user_id, title,
+permalink_url, track_type, state, created_at,
+original_format, description, sharing,
+genre, duration, key_signature, bpm,
+license, label_name,
+favoritings_count,
+streamable, stream_url,
+downloadable, download_count,
+commentable,
+purchase_url, artwork_url, video_url, embeddable_by,
+release, release_month, release_day, release_year, tag_list'''
+tracks_table_creator='''id INTEGER, user_id INTEGER, title TEXT,
+permalink_url TEXT,  track_type TEXT, state TEXT, created_at TEXT,
+original_format TEXT, description TEXT, sharing TEXT,
+genre TEXT, duration INTEGER, key_signature TEXT, bpm INTEGER,
+license TEXT, label_name TEXT,
+favoritings_count INTEGER,
+streamable TEXT, stream_url TEXT,
+downloadable TEXT, download_count INTEGER,
+commentable TEXT,
+purchase_url TEXT, artwork_url TEXT, video_url TEXT, embeddable_by TEXT,
+release TEXT, release_month INTEGER, release_day INTEGER, release_year INTEGER,
+tag_list TEXT'''
+
+
+def att_list(att_str):
+    return [att.strip() for att in att_str.split(',')]
+
+
+def obj_atts_list(obj, att_list):
+    l = []
+    for att in att_list:
+        try:
+            l.append(getattr(obj,att))
+        except AttributeError:
+            l.append('NULL')
+    return l
+
+
+def add_data(cursor, table, data, att_str, att_list):
+    sql='INSERT INTO {} VALUES({})'.format(table,('?, '*len(att_list))[:-2])
+
+    for datum in data:
+        try:
+            vals=tuple(obj_atts_list(datum,att_list))
+            cursor.execute(sql,vals)
+        except Exception as e:
+            print('Error adding {} to '
+                  '{}: {} {}'.format(datum.id,table,e.message,e.args))
+
+
+# Unit tests follow
+
+class placeholder():
+    pass
+
+
+def dummy_data():
+    ph1 = placeholder()
+    ph1.id = 12345
+    ph2 = placeholder()
+    ph2.id = 67890
+    ph2.user_id = 11102
+    return [ph1,ph2]
+
+
+def test1():
+    test_data = dummy_data()
+    conn = sqlite3.connect('test1.sqlite')
+    curs = conn.cursor()
+    curs.execute('DROP TABLE IF EXISTS dummy')
+    curs.execute('CREATE TABLE IF NOT '
+                 'EXISTS dummy({})'.format(dummy_table_creator))
+    add_data(curs,'dummy',test_data,dummy_att_str,att_list(dummy_att_str))
+    conn.commit()
+
+
+def test2():
+    test_data = dummy_data()
+    conn = sqlite3.connect('test2.sqlite')
+    curs = conn.cursor()
+
+    curs.execute('DROP TABLE IF EXISTS tracks')
+    curs.execute('CREATE TABLE IF NOT '
+                 'EXISTS tracks({})'.format(tracks_table_creator))
+
+    add_data(curs,'tracks',test_data,track_att_str,att_list(track_att_str))
+    conn.commit()
