be4788bca4a49b75fe2ddc4f6d2347ee57997d06
daniel-allington <daniel.allington@open.ac.uk>
2014-05-02 16:50:38 +0100

    Minimal snowball sampler now works
    This now collects data and saves it to an SQLite database. It uses a
    different approach than get_soundcloud_data: first, it only requests
    followers and followings, and bases the users and x_follows_y on those,
    ignoring anything else; second, it works in 'steps', basically the
    number of degrees of separation from a starting individual. The idea is
    that because we only get up to 50 follows and followings, we may need
    to keep re-sampling from the same starting points, but without
    necessarily wanting to go many degrees of separation away from them
    until we've covered their immediate networks (say, when the number of
    followers for each individual is higher than a certain proportion of
    the total that exist). So the next thing will be to write the code that
    queries the database and decides who to re-request data on. Also to
    start calling some of the other helpful functions in
    get_soundcloud_data.py, for example to introduce a time delay. At the
    moment, I've tried running this with just two steps from one of our
    interviewees, just once - I got 4264 follow relationships between 3185
    individuals with 101 calls to the Soundcloud API, which took well under
    a minute (the max would have been 10001 individuals, in the same amount
    of time).

--- get_minimal.py
+++ get_minimal.py
@@ -1,37 +1,133 @@
 import add_data as ad
 import get_soundcloud_data as gsd
+import sqlite3
+
+class DbHandler(object):
+    def __init__(self,db_name):
+        self.conn = sqlite3.connect(db_name)
+        self.curs = self.conn.cursor()
+
+    def create_table(self,table_name):
+        ad.create_table(self.curs,table_name)
+
+    def write(self,table_name,data):
+        ad.insert_tuple_data_set_into_DB(self.curs,table_name,data)
+        self.conn.commit()
+
+class DataHandler(object):
+    table = ''
+    vars = []
+
+    def __init__(self,db_handler,limit):
+        self.db_handler = db_handler
+        self.limit = limit
+        self.data = []
+
+    def clear(self):
+        self.data = []
+
+    def save(self):
+        self.db_handler.write(self.table,self.data)
+        self.clear()
+
+    def add_record(self,r):
+        if len(self.data) >= self.limit: self.save()
+
+
+class UserData(DataHandler):
+    table = 'users'
+    vars=ad.att_list(ad.att_string(ad.tables[table]))
+
+    def __init__(self,db_handler,limit=100):
+        self.collected = set([])
+        super(UserData,self).__init__(db_handler,limit)
+
+    def extract(self,d,k):
+        try:
+            return d[k]
+        except KeyError:
+            return None
+
+    def add_record(self,id,d):
+        self.data.append([self.extract(d,v) for v in UserData.vars])
+        self.collected.add(id)
+        super(UserData,self).add_record(d)
+
+    def update(self,d):
+        for k,v in d.iteritems():
+            self.add_record(k,d[k])
+
+
+class FollowData(DataHandler):
+    table = 'x_follows_y'
+    vars=ad.att_list(ad.att_string(ad.tables[table]))
+
+    def __init__(self,db_handler,limit=100):
+        super(FollowData,self).__init__(db_handler,limit)
+
+    def add_record(self,t):
+        self.data.append(t)
+        super(FollowData,self).add_record(t)
+
+    def update(self,s):
+        for t in s:
+            self.add_record(t)
+
 
 def user_dicts(resource):
     return {u.obj['id']:u.obj for u in resource.data}
 
+
 def starting_user(user_id):
     return gsd.client.get('/users/' + str(user_id)).obj
 
+
 def from_user(user_id):
-    user_id=str(user_id)
-    follows = user_dicts(gsd.client.get('/users/'+ user_id +'/followings'))
-    followed_by = user_dicts(gsd.client.get('/users/'+ user_id +'/followers'))
+    uid=str(user_id)
+    follows = user_dicts(gsd.client.get('/users/'+ uid +'/followings'))
+    followed_by = user_dicts(gsd.client.get('/users/'+ uid +'/followers'))
     x_follows_y=set([])
     for y in follows:
         x_follows_y.add((user_id,y))
     for x in followed_by:
         x_follows_y.add((x,user_id))
     follows.update(followed_by)
-    return x_follows_y,follows
+    return follows,x_follows_y
 
-def from_users(user_list,x_follows_y,user_data):
+
+def from_users(user_list):
+    user_data,x_follows_y = {},set([])
     for user in user_list:
-        xfy,us = from_user(user)
-        x_follows_y.update(xfy)
+        us,xfy = from_user(user)
         user_data.update(us)
-    return x_follows_y, user_data
+        x_follows_y.update(xfy)
+    return user_data, x_follows_y
 
-def test(start_at=55078931,steps=1):
-    user_data={start_at:starting_user(start_at)}
-    folls_collected=set([])
-    x_follows_y=set([])
+
+def snowb(start_at,user_data,follow_data,folls_collected,steps):
+    user_data.update({start_at:starting_user(start_at)})
     for i in range(steps):
-        to_collect = [u for u in user_data if u not in folls_collected]
-        x_follows_y,user_data = from_users(to_collect,x_follows_y,user_data)
+        to_collect = user_data.collected - folls_collected
+        ud,xfy = from_users(to_collect)
+        user_data.update(ud)
+        follow_data.update(xfy)
         folls_collected.update(to_collect)
-    return user_data,x_follows_y,folls_collected
+    user_data.save()
+    follow_data.save()
+    return folls_collected
+
+
+def collect(dbname,start_at,steps=1):
+    dbh = DbHandler(dbname)
+    dbh.create_table('users')
+    dbh.create_table('x_follows_y')
+    user_data = UserData(dbh,1)
+    follow_data = FollowData(dbh,1)
+    already_collected = set([])
+    folls_collected = snowb(start_at,user_data,follow_data,
+                            already_collected,steps)
+    return folls_collected
+
+def test(steps=1):
+    collect('testit5.sqlite',202195,steps=steps)
+
