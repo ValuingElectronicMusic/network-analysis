c00a832c77712b7103d3d86e1bbb186e922b4a13
Anna Jordanous <annajordanous@hotmail.com>
2014-06-30 16:35:21 +0100

    initial setting up of basic sql queries

--- analyseConnections.py
+++ analyseConnections.py
@@ -17,16 +17,22 @@ def connect_to_db(db_path):
 
 def run_sql_query(cursor, query):
    
-    curs.execute(query)
-    result = curs.fetchall()
+    cursor.execute(query)
+    result = cursor.fetchall()
     return result
 
+def get_track_creator(track_id):
+    query = 'SELECT user_id FROM tracks WHERE id = '+track_id
+    return run_sql_query(query)
 
 def main(db_path): 
-    data = pscd.data_holder(db_path)
-    pscd.printData(data)
-    entities = pscd.entity_holder(data)
-    pscd.printEntities(entities)
+#    data = pscd.data_holder(db_path)
+#    pscd.printData(data)
+#    entities = pscd.entity_holder(data)
+#    pscd.printEntities(entities)
+    cursor = connect_to_db(db_path)
+    result = run_sql_query(cursor, 'SELECT COUNT(*) from users')
+    print(result)
  
 
 if __name__ == '__main__':
