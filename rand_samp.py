11ea7631477670877576eb8292fb0a4608e38f8e
daniel-allington <daniel.allington@open.ac.uk>
2014-07-08 09:38:35 +0100

    rand_samp.py now backs up the databases
    rand_samp.py is now a bit more like get_soundcloud_data.py in that it
    backs up its database and pauses between batches. Batches are of 1000,
    and take about half an hour to collect. It would be speeded up by
    modifications to all_data_one_person.py to prevent re-collections of
    data on individual tracks and individual users but I haven't had time
    to do that yet. Maybe best to run it as it is, and leave it running.

--- rand_samp.py
+++ rand_samp.py
@@ -9,19 +9,19 @@
 import sqlite3
 import time
 import random
+import shutil
+import os.path
 import add_data as ad
 import all_data_one_person as adop
 
 
-db_path = 'rand_samp'
+db_dirpath = 'rand_samp'
+backup_dirpath = 'rand_samp/backup'
+max_batch = 1000
 
 
 def time_stamp():
     return time.strftime('%Y%m%d_%H%M')
-
-
-def connect(db_name):
-    return sqlite3.connect(db_path+'/'+db_name+'.sqlite')
 
 
 def create_tables(curs):
@@ -29,35 +29,49 @@ def create_tables(curs):
         ad.create_table(curs,tabl)
 
 
-Slackk = 202195
-Sephirot = 81070
-Sculpture = 261433 # Soundcloud website is 'tapebox'
-Ms_Skyrym = 15899888
-FAS = 55078931
-
-#   As of 7 July 2014, there are nearly 103380000 SoundCloud accounts
-
-def collect(sample_size=1,db=None):
-    
-    start_time=time_stamp()
-
-    if db:
-        conn=connect(db)
-        curs=conn.cursor()
-    else:
-        conn=connect('rand_samp_'+start_time)
-        curs=conn.cursor()
-        create_tables(curs)
-
+def collect_batch(curs,batch_size):
     collected, non_ids = 0,0
-
-    while collected < sample_size:
+    while collected < batch_size:
         try_id = random.randint(0,104000000)
         print 'Trying '+str(try_id)
         if adop.collect(curs,try_id): 
             collected += 1
         else: 
             non_ids += 1
+    return collected, non_ids
+
+
+#   As of 7 July 2014, there are nearly 103380000 SoundCloud accounts
+
+def collect(sample_size=1,db_name=None):
+    
+    start_time=time_stamp()
+
+    if db_name:
+        conn=sqlite3.connect(os.path.join(db_dirpath,db_name+'.sqlite'))
+        curs=conn.cursor()
+    else:
+        db_name='rand_samp_'+start_time
+        conn=sqlite3.connect(os.path.join(db_dirpath,db_name+'.sqlite'))
+        curs=conn.cursor()
+        create_tables(curs)
+
+    collected, non_ids = 0,0
+
+    while collected < sample_size:
+        uncollected = sample_size - collected
+        batch_size = (uncollected if uncollected <= max_batch else max_batch)
+        collection=collect_batch(curs,batch_size)
         conn.commit()
+        collected += collection[0]
+        non_ids += collection[1]
+        print '{} collected.'.format(collected)
+        print 'Backing up...'
+        shutil.copyfile(os.path.join(db_dirpath,
+                                     db_name+'.sqlite'),
+                        os.path.join(backup_dirpath,
+                                     db_name+'_back_'+time_stamp()+'.sqlite'))
+        print "Pausing 3 minutes. If you've been planning to interrupt, now's your chance."
+        time.sleep(180)
 
     return collected, non_ids, start_time, time_stamp()
